49f43be9a1049d4f7e8c0b9b494f2b86f8e423c0
Ro-bu <robin.raid1@gmail.com>
2022-01-30 20:13:32 +0200

    number button presses make numbers appear on calculator screen. img files moved to local

--- script.js
+++ script.js
@@ -8,3 +8,16 @@
 // FURHTER USE CLEAR ANSWER ROW
 // IF PRESS ANS USE PREVIOUS ANSWER FOR NEW CALCULATION
 // ELSE ADD NEW NUMBER INPUT TO CALCULATION ROW
+
+const calculationRow = document.querySelector("#answer-row");
+
+const answerRow = document.querySelector("#calculation-row");
+
+
+const numberButton = document.querySelectorAll(".number-button")
+numberButton.forEach( el => el.addEventListener("click", event =>{
+    let contentToAdd = event.target.textContent;
+    let currentContent = calculationRow.textContent;
+    let newContent = currentContent.concat(contentToAdd);
+    calculationRow.textContent = newContent;
+}));
